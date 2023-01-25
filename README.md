8357022a1df2339c66a50e1b0173c7ca3efe7e7d
stony <stonyphoenix@gmail.com>
2023-01-25 01:08:42 -0500

    asincrona23

---
+++ README.md
@@ -0,0 +1,12 @@
+# Demo23 material
+
+los componentes de material como se instalan con
+
+```
+ng add @angular/material
+```
+
+- Te preguntara algunas cosas como el tema de tu aplicación, las tipografías y otras cosas
+- Luego dentro de tu modulo principal debes de poner los imports de material que necesitas y 
+- Dentro de los imports del NgModule
+- Luego en el componente escribir la etiqueta que este dentro de [sitio de material](https://material.angular.io/components/categories).
---
+++ src/app/principal/asincrona/asincrona.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asincrona',
+  templateUrl: './asincrona.component.html',
+  styleUrls: ['./asincrona.component.css']
+})
+export class AsincronaComponent {
+  suma = false
+  resta = false
+  multi = false
+  divi = false
+
+  suma_re = 0
+  resta_re = 0
+  multi_re = 0
+  divi_re = 0
+
+  num1 = 0
+  num2 = 0
+
+  resultado = ""
+  resultado_res=""
+  resultado_mul=""
+  resultado_divi=""
+  ngOnInit(){
+    this.operacion()
+  }
+  operacion(){
+    console.log(this.suma)
+    if(this.suma == true){
+      let resulta = this.num1 + this.num2
+      this.resultado = " la suma es: " +resulta.toString() +" "
+    }else{
+      this.resultado = ""
+    }
+    if(this.resta == true){
+      let resulta = this.num1 - this.num2
+      this.resultado_res = " la diferencia es: " +resulta.toString() +" "
+    }else{
+      this.resultado_res = ""
+    }
+    if(this.multi == true){
+      let resulta = this.num1 * this.num2
+      this.resultado_mul = " la producto es: " +resulta.toString() +" "
+    }else{
+      this.resultado_mul = ""
+    }
+    if(this.divi == true){
+      let resulta = this.num1 / this.num2
+      this.resultado_divi = " la division es: " +resulta.toString() +" "
+    }else{
+      this.resultado_divi = ""
+    }
+  }
+}
